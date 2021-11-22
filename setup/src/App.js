288c960d6ca86038bef73198618de3e3a89fa51e
MaianneThornton <maianne.thornton@gmail.com>
2021-11-22 11:26:15 -0500

    Created people prop to utilize the array of objects in data.js

--- setup/src/App.js
+++ setup/src/App.js
@@ -2,13 +2,18 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <main>
-    <section className="container">
-      <h3>0 Birthday's Today</h3>
-      <List /> {/* Display List Component */}
-      <button onClick= {() => console.log('You Clicked Me')}>Clear all</button>
-    </section>
-  </main>;
+  const [people, setPeople] = useState(data)
+  {/* creating useState to access un-named array of objects in data.js */ }
+  return (
+    <main>
+      <section className="container">
+        <h3>{people.length} Birthday's Today</h3>
+        {/* Accessing the number of birthdays via the number of items (objects) in the array in the data.js file */}
+        <List people={people} /> {/* Display List Component w/ people state value included */}
+        <button onClick={() => console.log('You Clicked Me')}>Clear all</button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
