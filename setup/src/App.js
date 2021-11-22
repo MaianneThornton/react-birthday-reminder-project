ca46e1a7acd1b9f170f47e44589b1d74979ca29a
MaianneThornton <maianne.thornton@gmail.com>
2021-11-22 10:11:49 -0500

    Imported starter files. Ready to being project

---
+++ setup/src/App.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+import data from './data';
+import List from './List';
+function App() {
+  return <h2>The reminder project setup</h2>;
+}
+
+export default App;
