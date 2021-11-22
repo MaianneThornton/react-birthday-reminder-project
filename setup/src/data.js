c374ad48fa011be9f88a82519295a92ab0679094
MaianneThornton <maianne.thornton@gmail.com>
2021-11-22 11:53:11 -0500

    Revamped data to reflect celebs born on Nov 22

--- setup/src/data.js
+++ setup/src/data.js
@@ -1,37 +1,58 @@
 export default [
   {
     id: 1,
-    name: 'Bertie Yates',
-    age: 29,
+    name: 'Scarlett Johansson',
+    age: 37,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+      'https://m.media-amazon.com/images/M/MV5BMTM3OTUwMDYwNl5BMl5BanBnXkFtZTcwNTUyNzc3Nw@@._V1_UY317_CR23,0,214,317_AL_.jpghttps://www.imdb.com/name/nm0424060?ref_=hm_brn_1',
   },
   {
     id: 2,
-    name: 'Hester Hogan',
-    age: 32,
+    name: 'Mads Mikkelsen',
+    age: 56,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+      'https://m.media-amazon.com/images/M/MV5BMTcyMTU5MzgxMF5BMl5BanBnXkFtZTYwMDI0NjI1._V1_UX214_CR0,0,214,317_AL_.jpg',
   },
   {
     id: 3,
-    name: 'Larry Little',
-    age: 36,
+    name: 'Jamie Lee Curtis',
+    age: 63,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+      'https://m.media-amazon.com/images/M/MV5BMjI0MTI0MDk2OV5BMl5BanBnXkFtZTgwODQ4MTE0NDE@._V1_UY317_CR12,0,214,317_AL_.jpg',
   },
   {
     id: 4,
-    name: 'Sean Walsh',
-    age: 34,
+    name: "Auli'i Cravalho",
+    age: 21,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+      'https://m.media-amazon.com/images/M/MV5BMjQ0YTAwZDMtOWFhNy00ZDFjLTg1YjMtMTAyNmMyZTBjMzEwXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_UX214_CR0,0,214,317_AL_.jpg',
   },
   {
     id: 5,
-    name: 'Lola Gardner',
-    age: 29,
+    name: 'Mark Ruffalo',
+    age: 54,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+      'https://m.media-amazon.com/images/M/MV5BNWIzZTI1ODUtZTUzMC00NTdiLWFlOTYtZTg4MGZkYmU4YzNlXkEyXkFqcGdeQXVyNTExOTk5Nzg@._V1_UX214_CR0,0,214,317_AL_.jpg',
+  },
+  {
+    id: 5,
+    name: 'Alden Ehrenreich',
+    age: 32,
+    image:
+      'https://m.media-amazon.com/images/M/MV5BNjY5ODY0MDE5Nl5BMl5BanBnXkFtZTgwODg2NDM4ODE@._V1_UX214_CR0,0,214,317_AL_.jpg',
+  },
+  {
+    id: 5,
+    name: 'Richard Kind',
+    age: 65,
+    image:
+      'https://m.media-amazon.com/images/M/MV5BMjEwMzgyNTI0MF5BMl5BanBnXkFtZTYwMTQ0OTI4._V1_UX214_CR0,0,214,317_AL_.jpg',
+  },
+  {
+    id: 5,
+    name: 'Jamie Campbell Bower',
+    age: 33,
+    image:
+      'https://m.media-amazon.com/images/M/MV5BMTI4ODQ4OTExOF5BMl5BanBnXkFtZTcwMTUzMzc0Mw@@._V1_UY317_CR17,0,214,317_AL_.jpg',
   },
 ];
