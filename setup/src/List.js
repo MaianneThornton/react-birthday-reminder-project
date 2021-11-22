88705f46cb6f8b6a1137ca3981f5d695c16b4701
MaianneThornton <maianne.thornton@gmail.com>
2021-11-22 11:34:55 -0500

    created the displayed list items using the people prop created in app.js

--- setup/src/List.js
+++ setup/src/List.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
-const List = () => {
+const List = ({people}) => { // destructuring the people prop created in app.js
   return (
     <>
-      <h2>list component</h2>
+      {people.map((person) => {
+        const {id, name, age, image} = person;
+        // creating a new array (map method) with the returned values from the data.js file (id, name, age, image) and setting it to the person variable
+        return <article key={id} className="person">
+          <img src={image} alt={name} />
+          {/* setting the image to the images  and the alt to the names set in the data.js file */}
+          <div>
+            <h4>{name}</h4>
+            <p>{age} Years Old</p>
+          </div>
+        </article>
+      } )}
     </>
   );
 };
